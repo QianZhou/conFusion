016ffd73995edef4abf31e26a9d646aba537424f
QianZhou <qzhou@lamar.edu>
2016-03-14 19:43:02 -0500

    Angular Forms
    1. Construct the form for users to submit their comments.
    Set up the form with the three fields: name, rating and comment.
    Use a JavaScript object in the controller to keep track of the
    information.
    The rating should be set to 5 by default.
    Use radio for specifying the rating in the form.
    Use two-way data binding between the form fields and the JavaScript
    object.
    
    2. Enable form validation using Angular support for form validation.
    The name and comments are required fields and the user has entered
    appropriate information in both the fields.
    The submit button should be disabled until the user has entered all the
    required fields.
    The user should be alerted by showing the invalid fields in red, and a
    message displayed at the bottom of the field.
    
    3. Enable real-time preview of the user's comments on the web page.
    Show a preview just above the form using the same format as the regular
    comments on the page.
    Display the preview only if the user has entered valid information into
    the form, and the form is not in its pristine state.
    Upon submitting the valid comment, the comment should join the regular
    comments on the page. The date for the comment should be set
    automatically upon submission of the form.

--- app/scripts/app111.js
+++ app/scripts/app111.js
@@ -1,14 +1,12 @@
 'use strict';
-
 angular.module('confusionApp', [])
 
 .controller('MenuController', ['$scope', function ($scope) {
-
     $scope.tab = 1;
     $scope.filtText = '';
     $scope.showDetails = false;
 
-    $scope.dishes = [
+    var dishes = [
         {
             name: 'Uthapizza',
             image: 'images/uthapizza.png',
@@ -46,10 +44,10 @@ angular.module('confusionApp', [])
             comment: ''
                         }
                         ];
+    $scope.dishes = dishes;
 
     $scope.select = function (setTab) {
         $scope.tab = setTab;
-
         if (setTab === 2) {
             $scope.filtText = "appetizer";
         } else if (setTab === 3) {
@@ -61,6 +59,7 @@ angular.module('confusionApp', [])
         }
     };
 
+
     $scope.isSelected = function (checkTab) {
         return ($scope.tab === checkTab);
     };
@@ -68,10 +67,9 @@ angular.module('confusionApp', [])
     $scope.toggleDetails = function () {
         $scope.showDetails = !$scope.showDetails;
     };
-        }])
+}])
 
 .controller('ContactController', ['$scope', function ($scope) {
-
     $scope.feedback = {
         mychannel: "",
         firstName: "",
@@ -79,27 +77,21 @@ angular.module('confusionApp', [])
         agree: false,
         email: ""
     };
-
     var channels = [{
         value: "tel",
         label: "Tel."
-    }, {
+        }, {
         value: "Email",
         label: "Email"
-    }];
-
+        }];
     $scope.channels = channels;
     $scope.invalidChannelSelection = false;
-
-}])
+ }])
 
 .controller('FeedbackController', ['$scope', function ($scope) {
-
     $scope.sendFeedback = function () {
-
         console.log($scope.feedback);
-
-        if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+        if ($scope.feedback.agree && ($scope.feedback.mychannel == "") && !$scope.feedback.mychannel) {
             $scope.invalidChannelSelection = true;
             console.log('incorrect');
         } else {
@@ -112,88 +104,21 @@ angular.module('confusionApp', [])
                 email: ""
             };
             $scope.feedback.mychannel = "";
+
             $scope.feedbackForm.$setPristine();
             console.log($scope.feedback);
         }
     };
 }])
 
-.controller('DishDetailController', ['$scope', function ($scope) {
-
-    var dish = {
-        name: 'Uthapizza',
-        image: 'images/uthapizza.png',
-        category: 'mains',
-        label: 'Hot',
-        price: '4.99',
-        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-        comments: [
-            {
-                rating: 5,
-                comment: "Imagine all the eatables, living in conFusion!",
-                author: "John Lemon",
-                date: "2012-10-16T17:57:28.556094Z"
-                               },
-            {
-                rating: 4,
-                comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                author: "Paul McVites",
-                date: "2014-09-05T17:57:28.556094Z"
-                               },
-            {
-                rating: 3,
-                comment: "Eat it, just eat it!",
-                author: "Michael Jaikishan",
-                date: "2015-02-13T17:57:28.556094Z"
-                               },
-            {
-                rating: 4,
-                comment: "Ultimate, Reaching for the stars!",
-                author: "Ringo Starry",
-                date: "2013-12-02T17:57:28.556094Z"
-                               },
-            {
-                rating: 2,
-                comment: "It's your birthday, we're gonna party!",
-                author: "25 Cent",
-                date: "2011-12-02T17:57:28.556094Z"
-                               }
-
-            ]
+.controller('CommentController', ['$scope', function ($scope) {
+    $scope.comments = {
+        name: "",
+        rating: false,
+        comment: ""
     };
 
-    $scope.dish = dish;
-}])
-
-.controller('DishCommentController', ['$scope', function ($scope) {
-
-    //Step 1: Create a JavaScript object to hold the comment from the form
-    $scope.dishComment = {
-        rating: 5,
-        comment: "",
-        author: "",
-        date: ""
+    $scope.sendComment = function () {
+        console.log($scope.commments);
     };
-
-
-    $scope.submitComment = function () {
-
-        //Step 2: This is how you record the date
-        //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
-        $scope.dishComment.date = new Date().toISOString();
-
-        // Step 3: Push your comment into the dish's comment array
-        $scope.dish.comments.push($scope.dishComment);
-
-        //Step 4: reset your form to pristine
-        $scope.commentForm.$setPristine();
-
-        //Step 5: reset your JavaScript object that holds your comment
-        $scope.dishComment = {
-            rating: 5,
-            comment: "",
-            author: "",
-            date: ""
-        };
-    };
-}]);
+ }]);
